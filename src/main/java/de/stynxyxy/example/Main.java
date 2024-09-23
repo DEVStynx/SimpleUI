dc0e7cb225d6ba31828c3009d6c9d3aef8ac877c
DEVStynx <131193709+DEVStynx@users.noreply.github.com>
2024-09-23 20:02:13 +0200

    Update Main.java

--- src/main/java/de/stynxyxy/example/Main.java
+++ src/main/java/de/stynxyxy/example/Main.java
@@ -6,13 +6,27 @@ import de.stynxyxy.simpleui.ui.SimpleLayout;
 import de.stynxyxy.simpleui.ui.SimplePanel;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
 
         //ExampleUI ui =  new ExampleUI();
         SimpleGUI ui = new SimpleGUI("example",new SimplePanel(new SimpleBorder(100,100,100,100).get(), SimpleLayout.createSimpleRowLayout(1,1)));
-        ui.getPanel().addWidget(new JButton("test"));
+
+        JButton button = new JButton("test");
+        JLabel label = new JLabel("text");
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("pressed button");
+                label.setText("pressed the Button!");
+            }
+        });
+        ui.getPanel().addWidget(label);
+        ui.getPanel().addWidget(button);
         ui.update();
     }
 }
